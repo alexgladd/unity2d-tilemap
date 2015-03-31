3e8aa157720f2e4fc93a7dd0f6368ad3b8c7def2
Alex Gladd <gladdware@alexgladd.com>
2015-03-30 20:38:33 -0400

    Rename camera scaling component

---
+++ TileMap/Assets/Scripts/TileMap/TileScaleCamera.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class TileScaleCamera : MonoBehaviour {
+
+	[Tooltip("Tile size in pixels")]
+	public int tilePixelSize = 32;
+	
+	[Tooltip("Scaling factor for camera")]
+	public float scale = 2f;
+	
+	[Tooltip("Update scaling continuously?")]
+	public bool continuousUpdates = true;
+	
+	private Camera tileCam;
+	
+	void Awake () {
+		tileCam = GetComponent<Camera>();
+	}
+	
+	void Start () {
+		if (tileCam) {
+			tileCam.orthographic = true;
+			UpdateCameraScale();
+		}
+	}
+	
+	void Update () {
+		if (continuousUpdates && tileCam) {
+			UpdateCameraScale();
+		}
+	}
+	
+	void UpdateCameraScale () {
+		tileCam.orthographicSize = (Screen.height / tilePixelSize) / scale;
+	}
+}
